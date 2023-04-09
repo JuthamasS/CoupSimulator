311b0f6fc14c6da231a472f49ac863fc50c20b6f
Juthamas Saikaew <pyjamassamajyp@gmail.com>
2023-04-09 23:41:43 +0700

    update condition

--- MiNiGame/Program.cs
+++ MiNiGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MiNiGame
 {
@@ -29,12 +30,12 @@ namespace MiNiGame
             
             Program program = new Program();
 
-            //program.WelcomeText();
+            program.WelcomeText();
 
-            //program.playerName = program.GetPlayerName();
-            program.playerName = "Juthamaz";
+            program.playerName = program.GetPlayerName();
+            //program.playerName = "Juthamaz";
 
-            //program.GetCommand();
+            program.GetCommand();
 
             program.RandomRolesStartingGame();
 
@@ -121,7 +122,7 @@ namespace MiNiGame
                 Console.WriteLine("[ Waiting for Bot..   ]");
                 Console.WriteLine();
                 Console.WriteLine();
-                BotTurn();
+                BotTurn(false);
                 Console.WriteLine();
                 Console.WriteLine("                      >> Bot <<");
                 Console.WriteLine();
@@ -188,7 +189,7 @@ namespace MiNiGame
         }
 
         public void PrintAction(Boolean isChooseToLose,
-            Boolean ischooseToBlockStealBy,
+            Boolean isChooseToBlockStealBy,
             Boolean canChallenge,
             Boolean hasStealAction, 
             Boolean hasForeignAidAction, 
@@ -205,8 +206,12 @@ namespace MiNiGame
 
             if (player_coin > 6) Console.WriteLine("    cp - coup");
             if (player_coin > 2) Console.WriteLine("    as - assasinate");
-            
-            if (canChallenge) Console.WriteLine("     c - challenge");
+
+            if (canChallenge)
+            {
+                Console.WriteLine("     c - challenge");
+                Console.WriteLine("     p - pass");
+            }
             
             if (hasStealAction) Console.WriteLine("   bst - block stealing");
             
@@ -230,7 +235,7 @@ namespace MiNiGame
                 }
             }
 
-            if (ischooseToBlockStealBy)
+            if (isChooseToBlockStealBy)
             {
                 Console.WriteLine("    am - ambassador");
                 Console.WriteLine("    cp - captain");
@@ -238,13 +243,27 @@ namespace MiNiGame
         }
         
 
-        public void BotTurn()
+        public Boolean BotTurn(Boolean canChallenge)
         {
-            var bottAction = BotAction(false, false, false, false, false, false);
-            int num = random.Next(1,bottAction.Count);
+            Boolean resultTurn = false;
+            Boolean isChooseToLoseParam = false;
+            Boolean isChooseToBlockStealParam = false;
+            Boolean canChallengeParam = false;
+            Boolean hasStealActionParam = false;
+            Boolean hasForeignAidActtionParam = false;
+            Boolean hasAssasinateActtionParam = false;
+            
+            if (canChallenge)
+            {
+                canChallengeParam = true;
+            }
+            
+            var bottAction = BotAction(isChooseToLoseParam, isChooseToBlockStealParam, canChallengeParam, hasStealActionParam, hasForeignAidActtionParam, hasAssasinateActtionParam);
+            int num = random.Next(1, bottAction.Count);
             string bot_action = bottAction[num - 1];
-            var result = ProcessAction("Bot",bot_action,false);
-            var x = 1;
+            var result = ProcessAction("Bot", bot_action, false);
+
+            return resultTurn;
         }
 
         public string PlayerInput()
@@ -258,6 +277,7 @@ namespace MiNiGame
             Console.WriteLine();
             return ProcessAction(playerName,player_input,true);
         }
+
         public string ProcessAction(string name,string action_input,Boolean isPlayerTurn)
         {
             string result_return = "";
@@ -317,14 +337,63 @@ namespace MiNiGame
                     }
                     break;
                 case "t":
-                    Console.WriteLine(name + " take 3 coins");
                     if (isPlayerTurn)
                     {
                         player_coin += 3;
                     }
                     else
                     {
-                        bot_coin += 3;
+                        bot_action.Add("st");
+                        PrintAction(false, false, true, false, false, false);
+                    }
+                    break;
+                case "st":
+                    if (isPlayerTurn)
+                    {
+                        player_action.Add("st");
+                        BotTurn(true);
+                        Console.WriteLine(name + " take 2 coins from Bot");
+                        player_coin += 2;
+                    }
+                    else
+                    {
+                        bot_action.Add("st");
+                        PrintAction(false, true, true, false, false, false);
+                    }
+                    break;
+                case "p":
+                    if (isPlayerTurn)
+                    {
+
+                    }
+                    else
+                    {
+                        switch (player_action[(player_action.Count - 2)])
+                        {
+                            case "st":
+                                Console.WriteLine("Bot take 2 coins from " + name);
+                                bot_coin += 2;
+                                break;
+                            case "t":
+                                Console.WriteLine("Bot take 3 coins");
+                                bot_coin += 3;
+                                break;
+                            case "as":
+                                
+                                break;
+                            case "ex":
+                                
+                                break;
+                            case "bas":
+                                
+                                break;
+                            case "bst":
+                                
+                                break;
+                            case "bfa":
+                                
+                                break;
+                        }
                     }
                     break;
                 default:
@@ -368,7 +437,8 @@ namespace MiNiGame
             Console.WriteLine();
             Thread.Sleep(800);
             Console.Write("                                          Player name : ");
-            string playerName = Console.ReadLine();
+            string playerName = 
+                Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
